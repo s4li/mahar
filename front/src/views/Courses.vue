166c72e59b3e340720b87333a1e784ce25fa5ed0
soheilabdollahi <soheilabdollahi1375@yahoo.com>
2020-04-30 17:30:03 +0430

    some change on courses page

--- front/src/views/Courses.vue
+++ front/src/views/Courses.vue
@@ -1,75 +1,105 @@
 <template>
-<div>
-    <b-carousel id="carousel-1" v-model="slide" :interval="4000" controls indicators background="#ababab" img-width="1024" img-height="480" style="text-shadow: 1px 1px 2px #333;" @sliding-start="onSlideStart" @sliding-end="onSlideEnd">
-        <!-- Text slides with image -->
-        <b-carousel-slide caption="First slide" text="Nulla vitae elit libero, a pharetra augue mollis interdum." img-src="https://picsum.photos/1024/480/?image=52"></b-carousel-slide>
-
-        <!-- Slides with custom text -->
-        <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-            <h1>Hello world!</h1>
-        </b-carousel-slide>
-
-        <!-- Slides with image only -->
-        <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
-
-        <!-- Slides with img slot -->
-        <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-        <b-carousel-slide>
-            <template v-slot:img>
-                <img class="d-block img-fluid w-100" width="1024" height="480" src="https://picsum.photos/1024/480/?image=55" alt="image slot">
-            </template>
-        </b-carousel-slide>
-
-        <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-        <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-                a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-            </p>
-        </b-carousel-slide>
-    </b-carousel>
-
-    <p class="mt-4">
-        Slide #: {{ slide }}<br>
-        Sliding: {{ sliding }}
-    </p>
+<div class="position-relative">
+    <div class="back first show-box">
+        <h3>الغیم</h3>
+        <button class="btn play" @click.prevent="playSound('http://soundbible.com/mp3/Elevator Ding-SoundBible.com-685385892.mp3')"><i class="fas fa-volume"></i></button>
+        <button class="btn btn-warning">معنیش چیه؟</button>
+    </div>
+    <div class="back seconde ">
+        <h3>ابر</h3>
+        <div>
+            <button class="btn true"><i class="far fa-check"></i>درسته</button>
+            <button class="btn false"><i class="far fa-times"></i>غلطه</button>
+        </div>
+    </div>
 </div>
 </template>
 
 <script>
-import axios from 'axios'
+//import axios from 'axios'
 export default {
-    data() {
-        return {
-            Courses: [],
-            CoursesType: this.$route.params.Type,
-            slide: 0,
-            sliding: null
-        }
-    },
+    //data() {
+    //    return {
+    //        Courses: [],
+    //        CoursesType: this.$route.params.Type,
+    //
+    //    }
+    //},
     methods: {
-        getCourses() {
-            axios.get('/courses')
-                .then((res) => {
-                    this.Courses = res.data;
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-        },
-        onSlideStart() {
-            this.sliding = true
-        },
-        onSlideEnd() {
-            this.sliding = false
+        //    getCourses() {
+        //        axios.get('/courses')
+        //            .then((res) => {
+        //                this.Courses = res.data;
+        //            })
+        //            .catch((error) => {
+        //                console.error(error);
+        //            });
+        //    },
+        playSound(sound) {
+            if (sound) {
+                var audio = new Audio(sound);
+                audio.play();
+            }
         }
     },
-    created() {
-        this.getCourses();
-    },
+    //created() {
+    //    this.getCourses();
+    //},
 }
 </script>
 
 <style lang="css">
+.back {
+    padding: 15px;
+    margin-top: 30%;
+    background-color: #f8f9facc;
+    border-radius: 4px;
+    text-align: center;
+}
 
+.play {
+    display: block;
+    margin: 15px auto;
+    padding: 8px 12px;
+    border: 1px solid #909090;
+    background: #fff;
+}
+
+.play i {
+    vertical-align: middle;
+    font-size: 18px;
+}
+
+.first {}
+
+.seconde {}
+
+.true {
+    padding: 5px 15px;
+    background-color: #28a745;
+    margin: 10px 10px 0;
+    color: #ffffff;
+}
+
+.true i,
+.false i {
+    width: 15px;
+    text-align: right;
+    vertical-align: middle;
+}
+
+.false {
+    padding: 5px 15px;
+    background-color: #dc3545;
+    margin: 10px 10px 0;
+    color: #ffffff;
+}
+
+.show-box {
+    display: block;
+}
+
+.hidden-box {
+    display: none;
+}
 </style>
