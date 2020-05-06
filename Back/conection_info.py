c0d8850d71fb587a63e303111d5eb098ac21b0b4
parastoo.rambarzini <52406482+6273parastu@users.noreply.github.com>
2020-05-06 02:32:18 -0700

    db

--- Back/conection_info.py
+++ Back/conection_info.py
@@ -1,6 +1,6 @@
 import os
 
-host= os.environ.get('DOTEST_DB_HOST', 'localhost')
-db_name= os.environ.get('DOTEST_DB_NAME', 'mahar')
-mysql_user= os.environ.get('DOTEST_DB_USERNAME', 'root' )
-mysql_password=os.environ.get('DOTEST_DB_PASSWORD', 'Salam159')
+host= os.environ.get('MAHAR_DB_HOST', 'localhost')
+db_name= os.environ.get('MAHART_DB_NAME', 'mahar')
+mysql_user= os.environ.get('MAHAR_DB_USERNAME', 'root' )
+mysql_password=os.environ.get('MAHAR_DB_PASSWORD', 'Salam159')
