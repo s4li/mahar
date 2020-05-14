cd9b9dd462105d77b6dede3349d466d9dd36b8c9
soheilabdollahi <soheilabdollahi1375@yahoo.com>
2020-05-14 21:39:31 +0430

    City and grad selections were added to the grads page

--- front/src/views/Grades.vue
+++ front/src/views/Grades.vue
@@ -1,6 +1,7 @@
 <template>
 <div>
-    <Header title="پایه مورد نظر خود را انتخاب کنید"></Header>
+    <Header :title="title"></Header>
+    {{extraData}}
     <div class="parent">
         <transition name="fadeIn" mode="out-in">
             <div class="text-center bg-light p-4 rounded" v-if="toggleShow" key="first">
@@ -10,15 +11,34 @@
                     <button @click="onLogout" class="btn btn-danger mx-auto mt-3 shadow">خروج</button>
                 </div>
             </div>
-            <div class="btnBoxParrent" v-if="!toggleShow" key="seconde">
-                <h1 class="d-block h5">پایه مورد نظر خود را انتخاب کنید</h1>
-                <div class="btn-box">
-                    <router-link class="btn btn-warning" :class="{disabled:!grad.has_content}" :to="'/Lessons/'+ grad.id" v-for="(grad, index) in grads" :key="index">
-                        <i class="far fa-lock icon" :class="{'d-none':grad.has_content}"></i>
-                        پایه {{grad.title}}
-                    </router-link>
+            <div class="w-100" v-if="!toggleShow" key="seconde">
+                <div class="btnBoxParrent" v-if="extraData">
+                    <form @submit="getExtraUserData">
+                        <b-row class="mb-2">
+                            <b-col cols="2"><p class="titleextradata">شهر</p></b-col>
+                            <b-col>
+                                <b-form-select required class="my-2" v-model="city" :options="provinces"></b-form-select>
+                            </b-col>
+                        </b-row>
+                        <b-row class="mb-2">
+                            <b-col cols="2"><p class="titleextradata">پایه</p></b-col>
+                            <b-col>
+                                <b-form-select required class="my-2" v-model="gard" :options="degrees"></b-form-select>
+                            </b-col>
+                        </b-row>
+                        <button class="btn btn-primary" type="submit">ذخیره اطلاعات</button>
+                    </form>
                 </div>
-                <button @click="onLogout" class="btn btn-outline-danger mx-auto mt-3 d-block w-25 btn-sm shadow">خروج</button>
+                <div class="btnBoxParrent" v-if="!extraData">
+                    <h1 class="d-block h5">پایه مورد نظر خود را انتخاب کنید</h1>
+                    <div class="btn-box">
+                        <router-link class="btn btn-warning" :class="{disabled:!grad.has_content}" :to="'/Lessons/'+ grad.id" v-for="(grad, index) in grads" :key="index">
+                            <i class="far fa-lock icon" :class="{'d-none':grad.has_content}"></i>
+                            پایه {{grad.title}}
+                        </router-link>
+                    </div>
+                    <button @click="onLogout" class="btn btn-outline-danger mx-auto mt-3 d-block w-25 btn-sm shadow">خروج</button>
+                </div>
             </div>
         </transition>
     </div>
@@ -32,13 +52,7 @@ export default {
     components: {
         Header
     },
-    data() {
-        return {
-            grads: [],
-            toggleShow: true,
-            toolboxcondition: false
-        }
-    },
+
     methods: {
         onLogout() {
             this.$store.dispatch('logout')
@@ -46,11 +60,36 @@ export default {
         getgrads() {
             const url = this.$route.path
             this.$store.commit('StoreCurrentUrl', url);
-            axios.get('/courses')
+            axios.get('/courses', {
+                    params: {
+                        user_id: this.userId
+                    }
+                })
+                .then((res) => {
+                    this.grads = res.data.all_courses;
+                    this.extraData = res.data.complete_information
+                    if (this.extraData) {
+                        this.title = 'لطفا پایه و شهر خود را مشخص کنید'
+                        console.log(res.data.complete_information)
+                    } else {
+                        this.title = 'پایه مورد نظر خود را انتخاب کنید'
+                        console.log(this.extraData)
+                    }
+                    this.toggleShow = false
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+        getExtraUserData() {
+            axios.post('/information-completion-status', {
+                    user_id: this.userId,
+                    grade: this.grad,
+                    city: this.city,
+                })
                 .then((res) => {
                     console.log(res)
-                    this.grads = res.data;
-                    this.toggleShow = false
+                    this.extraData = false
                 })
                 .catch((error) => {
                     console.log(error);
@@ -66,11 +105,68 @@ export default {
             this.toggleCondition()
         }, 5000)
     },
+    data() {
+        return {
+            title:'',
+            grads: [],
+            toggleShow: true,
+            toolboxcondition: false,
+            userId: this.$store.state.userId,
+            gard: null,
+            city: null,
+            extraData: false,
+            provinces: [
+                { value: null, text: 'شهر خود را انتخاب نمایید' },
+                {"value": "آذربایجان شرقی", "text": "آذربایجان شرقی"},
+                {"value": "آذربایجان غربی", "text": "آذربایجان غربی"},
+                {"value": "اردبیل", "text": "اردبیل"},
+                {"value": "اصفهان", "text": "اصفهان"},
+                {"value": "البرز", "text": "البرز"},
+                {"value": "ایلام", "text": "ایلام"},
+                {"value": "بوشهر", "text": "بوشهر"},
+                {"value": "تهران", "text": "تهران"},
+                {"value": "چهارمحال و بختیاری", "text": "چهارمحال و بختیاری"},
+                {"value": "خراسان جنوبی", "text": "خراسان جنوبی"},
+                {"value": "خراسان رضوی", "text": "خراسان رضوی"},
+                {"value": "خراسان شمالی", "text": "خراسان شمالی"},
+                {"value": "خوزستان", "text": "خوزستان"},
+                {"value": "زنجان", "text": "زنجان"},
+                {"value": "سمنان", "text": "سمنان"},
+                {"value": "سیستان و بلوچستان", "text": "سیستان و بلوچستان"},
+                {"value": "فارس", "text": "فارس"},
+                {"value": "قزوین", "text": "قزوین"},
+                {"value": "قم", "text": "قم"},
+                {"value": "کردستان", "text": "کردستان"},
+                {"value": "کرمان", "text": "کرمان"},
+                {"value": "کرمانشاه", "text": "کرمانشاه"},
+                {"value": "کهگیلویه و بویراحمد", "text": "کهگیلویه و بویراحمد"},
+                {"value": "گلستان", "text": "گلستان"},
+                {"value": "گیلان", "text": "گیلان"},
+                {"value": "لرستان", "text": "لرستان"},
+                {"value": "مازندران", "text": "مازندران"},
+                {"value": "مرکزی", "text": "مرکزی"},
+                {"value": "هرمزگان", "text": "هرمزگان"},
+                {"value": "همدان", "text": "همدان"},
+                {"value": "یزد", "text": "یزد"}
+            ],
+            degrees: [
+                { value: null, text: 'پایه خود را انتخاب نمایید' },
+                {"value": "هفتم", "text": "هفتم"},
+                {"value": "هشتم", "text": "هشتم"},
+                {"value": "نهم", "text": "نهم"},
+                {"value": "دهم", "text": "دهم"},
+                {"value": "یازدهم", "text": "یازدهم"},
+                {"value": "دوازدهم", "text": "دوازدهم"},
+            ],
+        }
+    },
 }
 </script>
 
 <style scoped>
-
+.titleextradata{
+    margin: 15px 0 0 0;
+}
 .btn-box {
     display: flex;
     justify-content: space-between;
