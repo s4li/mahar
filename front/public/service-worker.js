e1962ee2346b0d98bdb99ebe7f2361897d9fb33b
soheilabdollahi <soheilabdollahi1375@yahoo.com>
2020-05-17 23:17:28 +0430

    guid page go offline axios defaults baseURL edit

--- front/public/service-worker.js
+++ front/public/service-worker.js
@@ -8,6 +8,16 @@ const staticAssets = [
   '../src/assets/fonts/fa-brands-400.woff',
   '../src/assets/fonts/fa-light-300.woff',
   '../src/assets/fonts/fa-regular-400.woff',
+  '../src/views/Guids.vue',
+  '../src/components/Header.vue',
+  '../src/components/Navbar.vue',
+  '../src/assets/image/guidImage/1.png',
+  '../src/assets/image/guidImage/2.jpg',
+  '../src/assets/image/guidImage/3.jpg',
+  '../src/assets/image/guidImage/4.jpg',
+  '../src/assets/image/guidImage/5.jpg',
+  '../src/assets/image/guidImage/6.jpg',
+  '../src/assets/image/guidImage/7.jpg',
 ];
 
 const cacheName = 'first';
