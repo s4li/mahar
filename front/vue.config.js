5b9db9e61e4caa6dfaa1ec347022bcca5565ad65
soheilabdollahi <soheilabdollahi1375@yahoo.com>
2020-05-19 18:47:47 +0430

    some another edit for pwa

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   pwa: {
     workboxPluginMode: 'InjectManifest',
+    //workboxPluginMode: 'GenerateSW',
     workboxOptions: {
         swSrc: 'public/service-worker.js',
         //navigateFallback: 'index.html',
