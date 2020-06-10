4f2bde667acecef37b8863b606ac0803dc817af6
s4li <v_raeisi156@yahoo.com>
2020-06-10 10:16:08 +0430

    Update conection_info.py

--- Back/conection_info.py
+++ Back/conection_info.py
@@ -3,7 +3,7 @@ import os
 host= os.environ.get('MAHAR_DB_HOST', 'localhost')
 db_name= os.environ.get('MAHART_DB_NAME', 'mahar')
 mysql_user= os.environ.get('MAHAR_DB_USERNAME', 'root' )
-mysql_password=os.environ.get('MAHAR_DB_PASSWORD', 'mysql_7621_*+-')
+mysql_password=os.environ.get('MAHAR_DB_PASSWORD', '')
 
 front_url = 'https://doplus.ir'
 back_url = 'https://doplus.ir'
