670fc6c7711dd3edbd1a3de0ba3fe57a070c933d
parastoo.rambarzini <52406482+6273parastu@users.noreply.github.com>
2020-07-12 04:00:56 -0700

    changs

--- Back/conection_info.py
+++
@@ -1,9 +0,0 @@
-import os
-
-host= os.environ.get('MAHAR_DB_HOST', 'localhost')
-db_name= os.environ.get('MAHART_DB_NAME', 'mahar')
-mysql_user= os.environ.get('MAHAR_DB_USERNAME', 'root' )
-mysql_password=os.environ.get('MAHAR_DB_PASSWORD', 'Dotest123456')
-
-front_url = 'https://doplus.ir'
-back_url = 'https://doplus.ir'
