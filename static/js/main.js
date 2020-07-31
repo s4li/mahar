739e1f9664cd4911dc98c03080e2fedc4b6bcc1c
soheilabdollahi <soheilabdollahi1375@yahoo.com>
2020-08-01 02:55:14 +0430

    The connection to the payment portal of the lessons page is over

--- static/js/main.js
+++ static/js/main.js
@@ -1,64 +1,105 @@
-$(document).ready(function() {
-    ("use strict");
+$(document).ready(function () {
+  ("use strict");
 
-    var window_width = $(window).width(),
-        window_height = window.innerHeight,
-        header_height = $(".default-header").height(),
-        fitscreen = window_height - header_height;
+  var window_width = $(window).width(),
+    window_height = window.innerHeight,
+    header_height = $(".default-header").height(),
+    fitscreen = window_height - header_height;
 
-    $(".fullscreen").css("height", window_height);
-    $(".fitscreen").css("height", fitscreen);
+  $(".fullscreen").css("height", window_height);
+  $(".fitscreen").css("height", fitscreen);
 
-    // Mobile Navigation
-    if ($("#nav-menu-container").length) {
-        var $mobile_nav = $("#nav-menu-container").clone().prop({
-            id: "mobile-nav",
-        });
-        $mobile_nav.find("> ul").attr({
-            class: "",
-            id: "",
-        });
-        $("body").append($mobile_nav);
-        $("body").prepend(
-            '<button type="button" id="mobile-nav-toggle"><i class="fas fa-bars"></i></button>'
-        );
-        $("body").append('<div id="mobile-body-overly"></div>');
-        $("#mobile-nav")
-            .find(".menu-has-children")
-            .prepend('<i class="fas fa-angle-down"></i>');
-
-        $(document).on("click", ".menu-has-children i", function(e) {
-            $(this).next().toggleClass("menu-item-active");
-            $(this).nextAll("ul").eq(0).slideToggle();
-            $(this).toggleClass("fa-angle-up fa-angle-down");
-        });
-
-        $(document).on("click", "#mobile-nav-toggle", function(e) {
-            $("body").toggleClass("mobile-nav-active");
-            $("#mobile-nav-toggle i").toggleClass("fa-times fa-bars");
-            $("#mobile-body-overly").toggle();
-        });
-
-        $(document).click(function(e) {
-            var container = $("#mobile-nav, #mobile-nav-toggle");
-            if (!container.is(e.target) && container.has(e.target).length === 0) {
-                if ($("body").hasClass("mobile-nav-active")) {
-                    $("body").removeClass("mobile-nav-active");
-                    $("#mobile-nav-toggle i").toggleClass("fa-times fa-bars");
-                    $("#mobile-body-overly").fadeOut();
-                }
-            }
-        });
-    } else if ($("#mobile-nav, #mobile-nav-toggle").length) {
-        $("#mobile-nav, #mobile-nav-toggle").hide();
-    }
-
-    // Header scroll class
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 100) {
-            $("#header").addClass("header-scrolled");
-        } else {
-            $("#header").removeClass("header-scrolled");
-        }
+  // Mobile Navigation
+  if ($("#nav-menu-container").length) {
+    var $mobile_nav = $("#nav-menu-container").clone().prop({
+      id: "mobile-nav",
     });
+    $mobile_nav.find("> ul").attr({
+      class: "",
+      id: "",
+    });
+    $("body").append($mobile_nav);
+    $("body").prepend(
+      '<button type="button" id="mobile-nav-toggle"><i class="fas fa-bars"></i></button>'
+    );
+    $("body").append('<div id="mobile-body-overly"></div>');
+    $("#mobile-nav")
+      .find(".menu-has-children")
+      .prepend('<i class="fas fa-angle-down"></i>');
+
+    $(document).on("click", ".menu-has-children i", function (e) {
+      $(this).next().toggleClass("menu-item-active");
+      $(this).nextAll("ul").eq(0).slideToggle();
+      $(this).toggleClass("fa-angle-up fa-angle-down");
+    });
+
+    $(document).on("click", "#mobile-nav-toggle", function (e) {
+      $("body").toggleClass("mobile-nav-active");
+      $("#mobile-nav-toggle i").toggleClass("fa-times fa-bars");
+      $("#mobile-body-overly").toggle();
+    });
+
+    $(document).click(function (e) {
+      var container = $("#mobile-nav, #mobile-nav-toggle");
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if ($("body").hasClass("mobile-nav-active")) {
+          $("body").removeClass("mobile-nav-active");
+          $("#mobile-nav-toggle i").toggleClass("fa-times fa-bars");
+          $("#mobile-body-overly").fadeOut();
+        }
+      }
+    });
+  } else if ($("#mobile-nav, #mobile-nav-toggle").length) {
+    $("#mobile-nav, #mobile-nav-toggle").hide();
+  }
+
+  // Header scroll class
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $("#header").addClass("header-scrolled");
+    } else {
+      $("#header").removeClass("header-scrolled");
+    }
+  });
+  /*******************************************************/
+  var contentWayPoint = function () {
+    var i = 0;
+    $(".animate-box").waypoint(
+      function (direction) {
+        if (
+          direction === "down" &&
+          !$(this.element).hasClass("animated-fast")
+        ) {
+          i++;
+          $(this.element).addClass("item-animate");
+          setTimeout(function () {
+            $("body .animate-box.item-animate").each(function (k) {
+              var el = $(this);
+              setTimeout(
+                function () {
+                  var effect = el.data("animate-effect");
+                  if (effect === "fadeIn") {
+                    el.addClass("fadeIn animated-fast");
+                  } else if (effect === "fadeInLeft") {
+                    el.addClass("fadeInLeft animated-fast");
+                  } else if (effect === "fadeInRight") {
+                    el.addClass("fadeInRight animated-fast");
+                  } else {
+                    el.addClass("fadeInUp animated-fast");
+                  }
+                  el.removeClass("item-animate");
+                },
+                k * 200,
+                "easeInOutExpo"
+              );
+            });
+          }, 100);
+        }
+      },
+      { offset: "85%" }
+    );
+  };
+  $(function () {
+    contentWayPoint();
+  });
 });
